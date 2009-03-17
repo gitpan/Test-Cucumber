9e9be1a2c1771af98f46301aca20ad12d0c95bf0
John Drago <jdrago_999@yahoo.com>
2009-03-17 18:14:47 +0000

    Import of JOHND/Test-Cucumber-0.001 from CPAN.
    gitpan-cpan-distribution: Test-Cucumber
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         JOHND/Test-Cucumber-0.001.tar.gz
    gitpan-cpan-author:       JOHND
    gitpan-cpan-maturity:     released

---
+++ lib/Test/Cucumber.pm
@@ -0,0 +1,48 @@
+
+package Test::Cucumber;
+
+use strict;
+use warnings 'all';
+
+our $VERSION = '0.001';
+
+1;# return true:
+
+=pod
+
+=head1 NAME
+
+Test::Cucumber - Perl extension for "Natural Language" test development
+
+=head1 SYNOPSIS
+
+  # TBD
+
+=head1 DESCRIPTION
+
+Cucumber is a testing concept that involves writing tests in a subset of English
+(or any language, really) that are then executed by a Cucumber Engine.
+
+=head2 Why is it called "Cucumber?"
+
+I'm not sure.  Yes I think it's a silly name.
+
+=head1 SEE ALSO
+
+Cucumber testing is really more of a Ruby concept.  The following links are of note:
+
+L<http://wiki.github.com/aslakhellesoy/cucumber>
+
+L<http://cukes.info/>
+
+=head1 AUTHOR
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 John Drago <jdrago_999@yahoo.com> - All Rights Reserved.
+
+This software is Free software and may be used and redistributed under
+the same terms as Perl itself.
+
+=cut
+
